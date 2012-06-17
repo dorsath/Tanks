126ffbb8e71722a3ca7ef6c80f1edf2eefbb70a9
Ronald Hecker <selsery@gmail.com>
2012-06-17 14:12:24 +0200

    Barrel turns with camera, but it's simply done by using the camera matrix thus its turning rather weird

--- lib/tanks/models/tanks/simple.rb
+++ lib/tanks/models/tanks/simple.rb
@@ -49,14 +49,15 @@ class SimpleTank
   end
 
   def turret
+    glTranslate(0,0.5,0)
+    glMultMatrix($camera.rotation.matrix.inverse * $camera.rotation.yaw(3.14))
     Draw.new(GL_TRIANGLE_STRIP) do |d|
       d.circle(0.05, 36) do |x, y|
-        d.vertex(x, 0.6 + y, 0)
-        d.vertex(x, 0.6 + y, 1.0)
+        d.vertex(x, y, 0)
+        d.vertex(x, y, 1.0)
       end
     end
 
-    glTranslate(0,0.5,0)
     glutSolidSphere(0.25,36,36)
 
     # Draw.new(GL_TRIANGLE_STRIP) do |d|
